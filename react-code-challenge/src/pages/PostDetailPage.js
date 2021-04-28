c3bd42cb3ae8b73956cf157c2b206e07d33ef191
Massi Marotta <massimarotta@gmail.com>
2021-04-28 02:29:32 -0300

    Added back button, navigation complete

--- react-code-challenge/src/pages/PostDetailPage.js
+++ react-code-challenge/src/pages/PostDetailPage.js
@@ -1,13 +1,15 @@
 import React, { useEffect } from 'react';
 import { useParams } from 'react-router-dom';
 import withMaster from '../components/hocs/withMaster';
+import { withRouter } from 'react-router-dom';
+import routesPaths from '../constants/routesPaths';
 import { func, object } from 'prop-types';
 import { connect } from 'react-redux';
 import { getPost } from '../redux/posts/postsActions';
 import Post from '../components/Post';
 import '../styles/pages/post-detail-page.scss';
 
-const PostDetailPage = ({ getPost, postDetail }) => {
+const PostDetailPage = ({ getPost, postDetail, history: { push } }) => {
   const { id: postId } = useParams();
 
   useEffect(() => {
@@ -19,7 +21,13 @@ const PostDetailPage = ({ getPost, postDetail }) => {
   return (
     <div className="post-detail-container">
       <div className="go-home">
-        <h3>{'<'} Back</h3>
+        <h3
+          onClick={() => {
+            push(routesPaths.home);
+          }}
+        >
+          {'<'} Back
+        </h3>
       </div>
       <Post title={title} id={id} userId={userId} body={body} isDetail />
     </div>
@@ -29,6 +37,7 @@ const PostDetailPage = ({ getPost, postDetail }) => {
 PostDetailPage.propTypes = {
   getPost: func.isRequired,
   postDetail: object.isRequired,
+  push: func.isRequired,
 };
 
 const mapState = ({ posts }) => ({
@@ -39,4 +48,4 @@ const mapDispatch = {
   getPost,
 };
 
-export default connect(mapState, mapDispatch)(withMaster(PostDetailPage));
+export default connect(mapState, mapDispatch)(withMaster(withRouter(PostDetailPage)));
